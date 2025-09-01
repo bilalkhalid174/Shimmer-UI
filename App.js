64607e95c7a3664dedb4509805fa22d968022507
Bilal khalid <142129510+bilalkhalid174@users.noreply.github.com>
2025-09-01 15:21:27 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,258 @@
+import React from "react";
+import "./ShimmerCard.css";
+
+
+const ShimmerCard = () => {
+  return (
+  <div className="main">
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+    <div className="shimmer-card">
+      <div className="shimmer-img"></div>
+      <div className="shimmer-lines">
+        <div className="shimmer-line"></div>
+        <div className="shimmer-line short"></div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default ShimmerCard;
